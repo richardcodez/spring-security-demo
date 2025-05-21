96ea4afd083448658fc0c9dff133385b898e5ce1
richardcodez <lebotsa.cr@gmail.com>
2025-05-21 10:02:18 +0200

    with static users in userDetailsService

--- src/main/java/com/richardcodez/spring_security_demo/config/SecurityConfig.java
+++ src/main/java/com/richardcodez/spring_security_demo/config/SecurityConfig.java
@@ -2,27 +2,74 @@ package com.richardcodez.spring_security_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+//    @Bean
+//    public AuthenticationProvider authProvider(){
+//
+//        // connects with database
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//
+//        return provider;
+//
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        // using anonymous classes
+
+//        Customizer<CsrfConfigurer<HttpSecurity>> customCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+//                configurer.disable();
+//            }
+//        };
+//
+//        http.csrf(customCsrf);
+
+        // using lambdas
+
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        http.formLogin(Customizer.withDefaults());
+  //        http.formLogin(Customizer.withDefaults());    // no longer needed for stateless
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                                .username("chite")
+                                .password("1234")
+                                .roles("USER")
+                                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                                .username("admin")
+                                .password("1234")
+                                .roles("ADMIN")
+                                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
