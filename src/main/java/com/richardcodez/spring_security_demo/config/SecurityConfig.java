39f348896271ca31ed05217a5e6735c3adbcd622
richardcodez <lebotsa.cr@gmail.com>
2025-05-22 11:11:50 +0200

    custom login works, not yet generating JWT token

--- src/main/java/com/richardcodez/spring_security_demo/config/SecurityConfig.java
+++ src/main/java/com/richardcodez/spring_security_demo/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.richardcodez.spring_security_demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -53,21 +55,27 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                                .username("chite")
-                                .password("1234")
-                                .roles("USER")
-                                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                                .username("admin")
-                                .password("1234")
-                                .roles("ADMIN")
-                                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
+
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                                .username("chite")
+//                                .password("1234")
+//                                .roles("USER")
+//                                .build();
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                                .username("admin")
+//                                .password("1234")
+//                                .roles("ADMIN")
+//                                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 
 }
--- src/main/java/com/richardcodez/spring_security_demo/controller/UserController.java
+++ src/main/java/com/richardcodez/spring_security_demo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.richardcodez.spring_security_demo.controller;
 import com.richardcodez.spring_security_demo.model.User;
 import com.richardcodez.spring_security_demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +16,30 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User register(@RequestBody User user){
         return service.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()) {
+            System.out.println("Successful Login");
+
+            return "Successful Login";
+        } else {
+            System.out.println("Failed to Login");
+
+            return "Failed to Login";
+        }
+
     }
 
 }
